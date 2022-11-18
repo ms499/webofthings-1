e6c3dd840cfe26917d9f29560b61574181f29c88
Monika Singh <monika@r061040.stusta.swh.mhn.de>
2022-11-18 15:11:40 +0100

    Event

--- producer.js
+++ producer.js
@@ -6,16 +6,21 @@ const {
 
 //const actionEmitter = require('./lib/emitter')
 actionFunc = require("./dist/actionHandler")
-
+eventFunc = require("./dist/eventHandler")
 ThingsConsume = require("./dist/thingsConsume")
 // Fetching the things description json from a file
 const tdJson= require('./td.json'); 
 // consuming the TD
 let garden = new ThingsConsume(tdJson);
 garden.setWriteProperty('state','manualWatering')
+
 // action call function
 actionFunc.invokeAction.on('startSprinkler', (thing) => {
   console.log("start the Sprinkler ")
+  let threshold = garden.getProperty('humidityThreshold').threshold
+  if (threshold > 20) {
+    eventFunc.emitEvent.emit('tooWet', {})
+  }
 })
 
 actionFunc.invokeAction.on('stopSprinkler', () => {
